0fdea7a69b5e7786061b4075af5812fe1c0e6eea
Chris Philbin <chris@chrisphilbin.net>
2019-02-12 00:34:08 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,10 @@ ON projects.id = pledges.project_id GROUP BY projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"Write your SQL query Here"
+"SELECT users.name, users.age
+FROM users
+LEFT JOIN pledges
+on user.id = pledges.user_id ASC"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
